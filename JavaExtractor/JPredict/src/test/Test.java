c8bd8e36a503192c645a569f1b08bf5bbc190d99
urialon <urialon1@gmail.com>
2020-07-15 10:24:39 +0300

    fig1.png

--- JavaExtractor/JPredict/src/test/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	void fooBar() {
-		if(xxxYyy > 1) {
-			getValue("Hello World");
-		}
-
-	}
-}
--- JavaExtractor/JPredict/src/test/TestLongPaths.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	void fooBar() {
-		do { do { do { do { do { if(xxxYyy > 1) {
-			getValue("Hello World");
-		} } while (true); } while (true); } while (true); } while (true); } while (true);
-		return xxxYyy;
-	}
-}
